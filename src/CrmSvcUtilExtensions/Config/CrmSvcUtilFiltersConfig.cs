8e3273792c6b6de2d6732be879268414b326b3ab
Kelvin Shen <kelvinshen@outlook.com>
2015-04-05 15:58:35 +1200

    initial setup

---
+++ src/CrmSvcUtilExtensions/Config/CrmSvcUtilFiltersConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CrmSvcUtilExtensions.Config
+{
+    [XmlRoot("crmsvcutilfilters")]
+    public class CrmSvcUtilFiltersConfig
+    {
+        [XmlElement("entities")]
+        public EntitiesFilter Entities { get; set; }
+
+        [XmlElement("optionsets")]
+        public OptionSetsFilter OptionSets { get; set; }
+    }
+}
